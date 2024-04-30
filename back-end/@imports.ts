aeddeeb56e0aca2092133a23321971b7e80f5bb8
Анна Севрюкова <a.sevrukova@getic.com>
2024-04-30 13:55:26 +0300

    .

--- back-end/@imports.ts
+++
@@ -1,9 +0,0 @@
-import 'standard'
-import 'defs'
-import 'utilities/@global'
-import 'helpers/@global'
-
-import 'events/update'
-
-import 'features/ECS.global'
-import 'features/Fc.global'
--- shared-game/@imports.ts
+++ shared-game/@imports.ts
@@ -1,9 +1,8 @@
 import '@defs'
 import 'standard'
 import 'helpers/global'
-import 'features/fc/global'
+import 'features/global'
 import 'features/ecs/global'
 import 'controllers/global'
-
 import 'events/animation-frame'
 
