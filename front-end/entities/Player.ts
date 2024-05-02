9f55c439a443f8b17b6ad8a3fc7d04ccec799e56
Анна Севрюкова <a.sevrukova@getic.com>
2024-05-02 14:41:46 +0300

    Fixes

--- front-end/entities/Player.ts
+++ front-end/entities/Player.ts
@@ -6,7 +6,7 @@ import Game from 'front-end/Front-End-Game'
 import BoxView from 'front-end/views/BoxView'
 import Vector3 from 'math/Vector3'
 
-@effect
+@entity
 export default class Player extends Entity {
     Position3Able: Position3Able
     Move3Able: Move3Able
@@ -16,16 +16,12 @@ export default class Player extends Entity {
     view: Three.Object3D
     wasdController: WasdController
 
-    constructor(deps: EffectDeps) {
-        super(deps)
-
+    onGameContext(): void {
         new Position3Able(this)
         new Move3Able(this)
         new Acceleration3Able(this)
         new LinearFriction3Able(this, percentsPerSecond(20))
-    }
 
-    onGameContext(): void {
         const { camera } = this.context(Game)
 
         const onControllersUpdate = (): void => {
