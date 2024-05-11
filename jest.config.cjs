32a426d2ac3124b96222a2c464572febe431c6af
Анна Севрюкова <anyasky91@yandex.ru>
2024-05-11 13:29:23 +0300

    Fixes

---
+++ jest.config.cjs
@@ -0,0 +1,21 @@
+const { pathsToModuleNameMapper } = require('ts-jest')
+
+const { compilerOptions } = require('./tsconfig')
+
+if (compilerOptions.paths['*'][0] === '*') {
+    compilerOptions.paths['*'][0] = './*'
+}
+compilerOptions.paths['./*'] = compilerOptions.paths['*']
+delete compilerOptions.paths['*']
+
+module.exports = {
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths),
+    modulePaths: ['<rootDir>'],
+    coverageDirectory: '../coverage',
+    moduleFileExtensions: ['js', 'jsx', 'json', 'ts', 'tsx'],
+    testRegex: '.*\\.test\\.ts$',
+    transform: { '^.+\\.(t|j)s$': 'ts-jest' },
+    collectCoverageFrom: ['**/*.(t|j)s'],
+}
