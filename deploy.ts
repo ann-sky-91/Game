17d6e1fb0634efb6715880341829c89462591eb4
Анна Севрюкова <anyasky91@yandex.ru>
2024-06-21 11:59:12 +0300

    Fix deploy

--- deploy.ts
+++ deploy.ts
@@ -21,15 +21,8 @@ function run(command: string, options: RunOptions = {}): void {
 }
 
 run('npm i')
-run('rm -rf ./modules')
-
-if (fs.statSync('./modules').isDirectory()) {
-    run('git pull --force')
-    run('npm i', { cwd: modulesPath })
-    run('npm audit fix', { cwd: modulesPath })
-    run('npm link', { cwd: modulesPath })
-    run('npm link sky')
-} else {
-    run('git clone https://github.com/annsky91/modules')
-    run('npm i', { cwd: modulesPath })
-}
+run('git clone https://github.com/annsky91/modules')
+run('npm i', { cwd: modulesPath })
+run('npm audit fix', { cwd: modulesPath })
+run('npm link', { cwd: modulesPath })
+run('npm link sky')
